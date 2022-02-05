8b5a018777b1e2ab0cac4981a073411a04a91bb7
ElisioWander <elisio741@hotmail.com>
2022-02-04 23:55:52 -0300

    saving-user-data

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,11 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+}
 
 //dados que serão passados para a função signIn
 type SignInCredentials = {
@@ -24,6 +30,8 @@ export const AuthContext = createContext({} as AuthContextData)
 //o provider que ficará em volta de toda a aplicação, possibilitando assim, o compartilhamento
 //das informações
 export function AuthProvider({children}: AuthProviderProps) {
+  const [user, setUser] = useState<User>()
+
   const isAuthenticated = false
 
   async function signIn({email, password}: SignInCredentials) {
@@ -31,6 +39,14 @@ export function AuthProvider({children}: AuthProviderProps) {
       const response = await api.post('sessions', {
         email,
         password
+      })
+
+      const { permissions, roles } = response.data
+
+      setUser({
+        email,
+        permissions,
+        roles,
       })
   
       console.log(response.data)
